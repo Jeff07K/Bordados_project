3bf2e22ea4bd08eea4f45cf1c7bad4c0430f0233
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-05-26 09:34:08 -0500

    update parche contraseñas

--- auth.py
+++ auth.py
@@ -1,3 +1,6 @@
+import bcrypt
+if not hasattr(bcrypt, '__about__'):
+    bcrypt.__about__ = type('about', (), {'__version__': bcrypt.__version__})()
 import os
 from datetime import datetime, timedelta
 from typing import Optional
